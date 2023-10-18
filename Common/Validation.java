f5df905225e29a753675aab43cb49cc128cd0a4e
levanhuy12345 <lehuy1234987@gmail.com>
2023-10-18 11:36:25 +0700

    calculateshape..

---
+++ Common/Validation.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Common;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PC
+ */
+public class Validation {
+    private final Scanner in = new Scanner(System.in);
+
+    public double checkInputDouble() {
+        while (true) {
+            try {
+                double result = Double.parseDouble(in.nextLine());
+                return result;
+            } catch (NumberFormatException e) {
+                System.err.println("Re-input");
+            }
+        }
+    }
+}
+
